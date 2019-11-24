b2af5dc68cffc7b64038a149a582c1bd30f8ed26
wright1 <prosperwright@yahoo.co.uk>
2019-11-24 12:11:27 +0000

    searchbar functionality add -quirk when only two results are recieved

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,13 @@
-var showItems = document.getElementsByClassName("carousel__image");
-var totalItems; //this is initially set to the length of arr displaying images
+let showItems = document.getElementsByClassName("carousel__image");
+let totalItems; //this is initially set to the length of arr displaying images
+let count = 0;
 
-
-
-const allProducts = arr => {
+const displayProducts = arr => {
+  const list = document.getElementsByClassName("carousel__container")[0];
+  //remove the previous list of images
+  while (list.firstChild) {
+    list.firstChild.remove();
+  };
   arr.forEach(element => {
     const list = document.getElementsByClassName("carousel__container")[0];
     //creation of html element
@@ -18,27 +22,52 @@ const allProducts = arr => {
       console.log("I've been clicked.");
     });
   });
-  totalItems = arr.length -1;
+  totalItems = arr.length - 1;
 };
 const setBtnEventlisteners = () => {
-  const prev = document.getElementsByClassName('carousel__btn--left')[0];
-  const next = document.getElementsByClassName('carousel__btn--right')[0];
+  const prev = document.getElementsByClassName("carousel__btn--left")[0];
+  const next = document.getElementsByClassName("carousel__btn--right")[0];
+  const search = document.getElementsByClassName("submit-btn")[0];
 
-  prev.addEventListener('click', movePrev);
-  next.addEventListener('click', moveNext);  
+  prev.addEventListener("click", movePrev);
+  next.addEventListener("click", moveNext);
+  search.addEventListener("click", searchResults);
 };
 const moveNext = () => {
+  if (count === showItems.length - 2) return;
   showItems[totalItems].style.transform = `translateX(${300}px)`;
-  totalItems--; 
+  totalItems--;
+  count++;
 };
 const movePrev = () => {
-  showItems[totalItems +1].style.transform = `translateX(-${5}px)`;
+  if (count === 0) return;
+  showItems[totalItems + 1].style.transform = `rotateY(-${5}deg)`;
   totalItems++;
+  count--;
+};
+const searchResults = () => {
+  let input = document.getElementsByTagName("input")[0];
+  //convert the user input to Title case
+  const toTitleCase = phrase => {
+    return phrase
+      .toLowerCase()
+      .split(" ")
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+  let result = toTitleCase(input.value);
+  //filter productDataWomens based on user input
+  const filterEle = ele => {
+    return ele.productTitle.includes(result);
+  };
+  const resultArr = productDataWomens.filter(filterEle);
+  console.log(resultArr);
+  //display the results
+  displayProducts(resultArr);
 };
 window.onload = () => {
   //render all images on screen
-  allProducts(productDataWomens);
+  displayProducts(productDataWomens);
   //set EventListeners on buttons
   setBtnEventlisteners();
 };
-
