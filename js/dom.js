82edd77dcff458b8eca358e014507525d70d01a7
wright1 <prosperwright@yahoo.co.uk>
2019-11-26 08:37:11 +0000

    add popup

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,8 @@
 let showItems = document.getElementsByClassName("carousel__image");
-let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+let width = Math.max(
+  document.documentElement.clientWidth,
+  window.innerWidth || 0
+);
 let totalItems; //this is initially set to the length of arr displaying images
 let slide = 1;
 let widthMatch = window.matchMedia("(min-width: 1024px)");
@@ -10,7 +13,7 @@ const displayProducts = arr => {
   //remove the previous list of images
   while (list.firstChild) {
     list.firstChild.remove();
-  };
+  }
   arr.forEach((element, i) => {
     const list = document.getElementsByClassName("carousel__container")[0];
     //creation of html element
@@ -19,36 +22,41 @@ const displayProducts = arr => {
     listItem.setAttribute("class", "carousel__image");
     listItem.setAttribute("src", element.imageSrc);
     listItem.setAttribute("alt", element.productTitle);
-    if(i< 4) listItem.className += " result";
+    if (i < 4) listItem.className += " result";
     //append elements
     list.appendChild(listItem);
-    listItem.addEventListener("click", (e) => {
-      console.log("I've been clicked.");
-      popUp(e)
-    },false);
+    listItem.addEventListener(
+      "click",
+      e => {
+        console.log("I've been clicked.");
+        popUp(e);
+      },
+      false
+    );
   });
   totalItems = arr.length - 1;
 };
-const initialDisplay = (arr) => {
-  arr[0].style.display = 'initial';
-  arr[1].style.display = 'initial';
-  arr[2].style.display = 'initial';
-}
+const initialDisplay = arr => {
+  arr[0].style.display = "initial";
+  arr[1].style.display = "initial";
+  arr[2].style.display = "initial";
+};
 const setBtnEventlisteners = () => {
   const prev = document.getElementsByClassName("carousel__btn--left")[0];
   const next = document.getElementsByClassName("carousel__btn--right")[0];
   const search = document.getElementsByClassName("submit-btn")[0];
-  
-  
-  if(width >= 1024){
+  const close = document.getElementsByClassName("popup__close")[0];
+
+  if (width >= 1024) {
     prev.addEventListener("click", fullScreenMovePrev);
     next.addEventListener("click", fullScreenMoveNext);
   } else {
     prev.addEventListener("click", movePrev);
     next.addEventListener("click", moveNext);
   }
-  
+
   search.addEventListener("click", searchResults);
+  close.addEventListener("click", closeModal);
 };
 const moveNext = () => {
   if (count === showItems.length - 2) return;
@@ -59,21 +67,21 @@ const moveNext = () => {
 const fullScreenMoveNext = () => {
   console.log("The morning has started Next");
   //if the value of slide +/- the length of the showItems array we reset it to 0
-  if(slide > (showItems.length-2) || slide < 1) slide = 1;
+  if (slide > showItems.length - 2 || slide < 1) slide = 1;
   console.log(showItems.length);
   let current = showItems[slide];
   let added = slide + 2;
-  console.log('This is added', added);
-  current.style.display = 'initial';
-  if(slide > 1)showItems[slide-2].style.display = 'none'
-  current.previousSibling.style.display = 'none';
-  current.nextSibling.style.display = 'initial';
-  showItems[added].style.display = 'initial';
-  console.log('This is the first', slide);
-  slide += 2; 
-  console.log('This is the second slide', slide);
-  console.log(showItems); 
-}
+  console.log("This is added", added);
+  current.style.display = "initial";
+  if (slide > 1) showItems[slide - 2].style.display = "none";
+  current.previousSibling.style.display = "none";
+  current.nextSibling.style.display = "initial";
+  showItems[added].style.display = "initial";
+  console.log("This is the first", slide);
+  slide += 2;
+  console.log("This is the second slide", slide);
+  console.log(showItems);
+};
 const movePrev = () => {
   if (count === 0) return;
   showItems[totalItems + 1].style.transform = `rotateY(-${5}deg)`;
@@ -90,14 +98,13 @@ const fullScreenMovePrev = () => {
   //   current.style.display = 'initial';
   //   current.nextSibling.style.display = 'initial';
   // }
-  
+
   console.log(current, slide);
-  if(slide > 1)showItems[slide-2].style.display = 'none'
-  current.previousSibling.style.display = 'initial';
-  current.style.display = 'initial';
-  current.nextSibling.style.display = 'initial';
-  
-}
+  if (slide > 1) showItems[slide - 2].style.display = "none";
+  current.previousSibling.style.display = "initial";
+  current.style.display = "initial";
+  current.nextSibling.style.display = "initial";
+};
 const searchResults = () => {
   let input = document.getElementsByTagName("input")[0];
   //convert the user input to Title case
@@ -118,26 +125,61 @@ const searchResults = () => {
   //display the results
   displayProducts(resultArr);
   //empty search bar
-  input.value ="";
+  input.value = "";
 };
-const popUp = (e) => {
+const popUp = e => {
+  console.log('choa',popUp.firstChild);
+  
+ 
   const product = e.target.alt;
   const findImage = ele => {
-     return ele.productTitle === product
+    return ele.productTitle === product;
+  };
+  const resultObj = productDataWomens.filter(findImage)[0];
+  const popUpDiv = document.getElementsByClassName("popup")[0];
+  const popupLink = document.getElementsByClassName("popup__link")[0];
+  const popupTitle = document.getElementsByClassName("popup__title")[0];
+  const popupPrice = document.getElementsByClassName("popup__price")[0];
+  const imageIMG = document.getElementsByClassName("popup__image")[0];
+  const image = document.createElement("img");
+  const imageDiv = document.createElement("div");
+  const title = document.createTextNode(resultObj.productTitle);
+  const price = document.createTextNode(resultObj.price);
+
+
+  if(popupPrice.firstChild){
+  popupLink.href = "";
+  popupTitle.firstChild.remove();
+  popupPrice.firstChild.remove();
+  imageIMG.remove();
   }
-  const resultObj = productDataWomens.filter(findImage);
-
-  console.log(resultObj)
   
+  imageDiv.setAttribute('class', 'popup__image')
+  image.setAttribute('alt', e.target.alt);
+  image.setAttribute('src', resultObj.imageSrc);
+  popupLink.href = resultObj.productUrl;
 
-  console.log('does it get in here', e.target.alt);
+  
+  popupTitle.append(title);
+  popupPrice.append(price);
+  popUpDiv.appendChild(imageDiv);
+  imageDiv.appendChild(image);
 
+  popUpDiv.style.display = 'initial';
+
+  console.log(resultObj);
+
+  console.log("does it get in here", e.target.alt);
+};
+const closeModal = () => {
+  const btn = document.getElementsByClassName("popup__close")[0].parentNode;
+  btn.parentNode.style.display = 'none';
 }
-window.onload = () => {  
+window.onload = () => {
   //render all images on screen
   displayProducts(productDataWomens);
   //set EventListeners on buttons
   setBtnEventlisteners();
   //for fullscreen - set 3 images to display
-  if(width >= 1024) initialDisplay(showItems);
+  if (width >= 1024) initialDisplay(showItems);
 };
